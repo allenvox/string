c595363f128caa5158c019efd70a77249a8292e9
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-05 11:14:15 +0700

    Edit test program

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main()
     string s1("hello");
     std::cout << s1 << std::endl;
     string s2("world");
-    std::cout << s1 + " " + s2 << std::endl;
+    std::cout << s2 << std::endl;
+    string s3("damn!");
+    std::cout << s3 << std::endl;
+    std::cout << s1 + " " + s2 + " " + s3 << std::endl;
     exit(0);
 }
