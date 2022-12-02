3c38eed77d4864058a1fc783090456e103733345
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-02 18:01:06 +0700

    Modify test program to show more features of a string class

--- main.cpp
+++ main.cpp
@@ -3,16 +3,24 @@
 
 int main()
 {
-    string s1("hello");
+    string s1("");
+    s1.set("hello");
     std::cout << s1 << std::endl;
+
     string s2("world");
     std::cout << s2 << std::endl;
+
     string s3("!!!");
     std::cout << s3 << std::endl;
+
     s3 = "...";
     std::cout << s3 << std::endl;
+
     std::cout << "(s1 == s2) = " << std::noboolalpha << (s1 == s2) << std::boolalpha << std::endl;
+
     std::cout << s1 + " " + s2 + s3 << std::endl;
-    std::cout << s2.append(" is great!") << std::endl;
+
+    s2.append(" is ");
+    std::cout << s2.append("great!") << std::endl;
     exit(0);
 }
