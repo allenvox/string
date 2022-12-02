13e5b2f0df43b0f3effe5c96c4e4ccbc3f976751
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-02 19:18:28 +0700

    Add comments to printing the string results out

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@
 int main()
 {
     string s1("");
+    std::cout << "s1 = " << s1 << std::endl;
+
     s1.set("hello");
-    std::cout << s1 << std::endl;
+    std::cout << "s1 = " << s1 << std::endl;
 
     string s2("world");
-    std::cout << s2 << std::endl;
+    std::cout << "s2 = " << s2 << std::endl;
 
     string s3("!!!");
-    std::cout << s3 << std::endl;
+    std::cout << "s3 = " << s3 << std::endl;
 
     s3 = "...";
-    std::cout << s3 << std::endl;
+    std::cout << "s3 = " << s3 << std::endl;
 
-    std::cout << "(s1 == s2) = " << std::noboolalpha << (s1 == s2) << std::boolalpha << std::endl;
+    std::cout << "(s1 == s2) = " << std::noboolalpha << (s1 == s2) << std::boolalpha << "(not equal)" << std::endl;
 
-    std::cout << s1 + " " + s2 + s3 << std::endl;
+    std::cout << "s1 + s2 + s3 = " << s1 + " " + s2 + s3 << std::endl;
 
     s2.append(" is ");
-    std::cout << s2.append("great!") << std::endl;
+    std::cout << "s2.append('is').append('great!') = " << s2.append("great!") << std::endl;
     exit(0);
 }
